327892b362873e3cf178eed8df041640c5a15232
Nikolay Sibekin <nik-sibekin@yandex.ru>
2022-12-24 08:24:13 +0300

    2 project

---
+++ projects/chat/ui/userName.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+export default class UserName {
+    constructor(element) {
+        this.element = element;
+    }
+
+    set(name) {
+        this.name = name;
+        this.element.textContent = name;
+    }
+
+    get() {
+        return this.name;
+    }
+}
+
